7f709d2d355c4837ab2669e890c84733e68783ae
GuipOliveira <guipeoliveira@gmail.com>
2017-04-24 15:26:22 -0300

    Porta animadas!

---
+++ Assets/Scripts/ChecarMaterial.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChecarMaterial : MonoBehaviour {
+
+    public GameObject GO;
+    public Material M;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "BolaVerde")
+        {
+            print("funcionou!!");
+        }
+    }
+
+
+}
