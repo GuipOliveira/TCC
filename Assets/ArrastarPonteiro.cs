2706ac41e293290254386b115237b50f05b19d08
unknown <guipeoliveira@gmail.com>
2017-10-30 01:16:43 -0200

    Puzzle 3

---
+++ Assets/ArrastarPonteiro.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrastarPonteiro : MonoBehaviour {
+
+    public Ponteiro getTarget;
+    bool isMouseDragging;
+    Vector3 offsetValue;
+    Vector3 positionOfScreen;
+    ArrastarLaser _pegavel;
+    RaycastHit hit;
+
+    void Update()
+    {
+
+        //Mouse Button Press Down
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hitInfo;
+            getTarget = ReturnClickedObject(out hitInfo);
+            hit = hitInfo;
+            if (getTarget != null)
+            {
+                getTarget.transform.Rotate(Vector3.right , -hit.point.x);
+            }
+
+        }
+
+
+
+    }
+
+    //Method to Return Clicked Object
+    Ponteiro ReturnClickedObject(out RaycastHit hit)
+    {
+        Ponteiro target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 20, out hit, 40))
+        {
+            target = hit.transform.GetComponent<Ponteiro>();
+        }
+        return target;
+    }
+
+}
