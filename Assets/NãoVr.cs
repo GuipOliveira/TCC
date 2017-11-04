884954b6e71dd54afe84eb139d8b6cb686c4da4d
lehgui <guimaraesmiranda@live.com>
2017-11-03 23:13:25 -0200

    ranking

---
+++ Assets/NãoVr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class NãoVr : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+        StartCoroutine(AtivarVr("cardboard"));
+    }
+	
+    public IEnumerator AtivarVr(string name)
+    {
+        UnityEngine.XR.XRSettings.LoadDeviceByName(name);
+        yield return null;
+        UnityEngine.XR.XRSettings.enabled = true;
+    }
+}
